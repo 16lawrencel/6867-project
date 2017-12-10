2a2c804ddb6d5c595070b6f588876d10e20ef26c
Lawrence <lawrence@dhcp-18-189-59-60.dyn.mit.edu>
2017-12-10 12:40:53 -0500

    probably doesn't work

--- a3c.py
+++ a3c.py
@@ -10,20 +10,24 @@ import gym, time, random, threading, os, pickle
 
 from skimage.transform import resize
 from skimage.color import rgb2gray
+from scipy.misc import imresize
 
 from collections import deque
 
 import matplotlib.pyplot as plt
 
 # constants
-ENV = 'TimePilot-v0'
+ENV = 'Pong-v0'
 SAVE_DIR = ENV + '-ckpts-a3c'
 SAVE_PARAM_PATH = SAVE_DIR + '/params'
 
-NUM_ACTIONS = None # we'll change this later
+SHOW_ENV_TEST = False
+RUN_TRAIN = True
+
+NUM_ACTIONS = 3 # we'll change this later
 
 RUN_TIME = 100000
-THREADS = 16
+THREADS = 8
 OPTIMIZERS = 1
 THREAD_DELAY = 0.001
 
@@ -39,10 +43,11 @@ EPS_STOP_DIST = [0.4, 0.3, 0.3]
 EPS_STEPS = 400000
 
 BATCH_SIZE = 32
-TRAIN_SIZE = 10000 # updates ocassionally
-FRAME_SKIP = 4
+TRAIN_SIZE = 1024 # updates ocassionally
+SAVE_FREQ = 20 # how frequently do we save
+FRAME_SKIP = 1
 LEARN_RATE = 7e-4
-LEARN_STEPS = 30000000
+LEARN_STEPS = 100000000
 RMS_DECAY = 0.99
 
 LOSS_V = 0.5 # v loss coefficeint
@@ -52,6 +57,7 @@ class Brain:
     def __init__(self):
         self.train_queue = []
         self.lock_queue = threading.Lock()
+        self.save_count = SAVE_FREQ
 
         self.graph = tf.Graph()
         self.graph.as_default()
@@ -95,7 +101,7 @@ class Brain:
         """
         # input is 84 x 84 x 4, as per the dqn paper
         # the first index represents batch size
-        self.s_t = tf.placeholder(tf.float32, shape = [None, 84, 84, 4])
+        self.s_t = tf.placeholder(tf.float32, shape = [None, 80, 80, 4])
         self.a_t = tf.placeholder(tf.float32, shape = [None, NUM_ACTIONS])
         self.r_t = tf.placeholder(tf.float32, shape = [None, 1])
         self.learn_rate = tf.placeholder(tf.float32, shape = [])
@@ -109,7 +115,6 @@ class Brain:
                 strides = 4, 
                 padding = 'same', 
                 activation = tf.nn.relu)
-        assert(conv1.shape[1:4] == [21, 21, 16])
 
         # input tensor shape: [batch_size, 21, 21, 16]
         # output tensor shape: [batch_size, 11, 11, 32]
@@ -120,12 +125,11 @@ class Brain:
                 strides = 2, 
                 padding = 'same', 
                 activation = tf.nn.relu)
-        assert(conv2.shape[1:4] == [11, 11, 32])
 
         # flatten tensor into a batch of vectors
         # input tensor shape: [batch_size, 11, 11, 32]
         # output tensor shape: [batch_size, 11 * 11 * 32]
-        conv2_flat = tf.reshape(conv2, [-1, 11 * 11 * 32])
+        conv2_flat = tf.layers.flatten(conv2)
 
         # input tensor shape: [batch_size, 11 * 11 * 32]
         # output tensor shape: [batch_size, 256]
@@ -133,7 +137,6 @@ class Brain:
                 inputs = conv2_flat, 
                 units = 256, 
                 activation = tf.nn.relu)
-        assert(dense.shape[1] == 256)
 
         # input tensor shape: [batch_size, 256]
         # output tensor shape: [batch_size, num_actions]
@@ -162,29 +165,15 @@ class Brain:
 
         grads, tvars = zip(*optimizer.compute_gradients(loss_total))
         clipped_grads, _ = tf.clip_by_global_norm(grads, 40)
+        clipped_grads = list(map(lambda x : -x, clipped_grads))
         self.minimize = optimizer.apply_gradients(zip(clipped_grads, tvars))
-
-        '''
-        #WIP
-        optimizer = tf.train.AdamOptimizer(self.learn_rate)
-        grads, tvars = zip(*optimizer.compute_gradients(loss_total))
-
-        lens = [4096, 16, 8192, 32, 991232, 256, 1536, 6, 256, 1]
-        grads = [tf.reshape(grads[i], [lens[i]]) for i in range(10)]
-        tvars = [tf.reshape(tvars[i], [lens[i]]) for i in range(10)]
-        print(grads[0].shape)
-        print(tvars[0].shape)
-        clipped_grads, _ = tf.clip_by_global_norm(grads, 40)
-        print(clipped_grads[0].shape)
-        self.minimize = optimizer.apply_gradients(zip(clipped_grads, tvars))
-        '''
 
     def get_learn_rate(self):
         if self.num > LEARN_STEPS: return 0
         return LEARN_RATE * (1 - float(self.num) / LEARN_STEPS)
 
     def optimize(self):
-        if len(self.train_queue) < TRAIN_SIZE:
+        if len(self.train_queue) < 32:
             time.sleep(0) # yield
             return
 
@@ -198,7 +187,7 @@ class Brain:
             self.train_queue = []
 
         print("Start training network...{}".format(self.num))
-        random.shuffle(train_queue) # decrease correlation
+        #random.shuffle(train_queue) # decrease correlation
         print("LENGTH: ", len(train_queue))
         print("LEARN RATE: ", self.get_learn_rate())
         s, a, r, s_, done = zip(*train_queue)
@@ -218,7 +207,10 @@ class Brain:
 
             self.session.run(self.minimize, feed_dict = {self.s_t: s[i:end], self.a_t: a[i:end], self.r_t: r_, self.learn_rate: self.get_learn_rate()})
 
-        self.save_graph()
+        self.save_count -= 1
+        if self.save_count < 0:
+            self.save_graph()
+            self.save_count = SAVE_FREQ
 
         print("Finished training network!")
 
@@ -227,10 +219,8 @@ class Brain:
         # this does mean that there are inefficiencies here
         if len(self.train_queue) >= TRAIN_SIZE: return
 
-        with self.lock_queue:
-            self.train_queue.append((s, a, r, s_, int(done)))
-            self.num += 1
-            #print(self.num)
+        self.train_queue.append((s, a, r, s_, int(done)))
+        self.num += 1
 
     def predict(self, s):
         return self.session.run([self.out_actions, self.out_value], feed_dict = {self.s_t: s})
@@ -242,14 +232,15 @@ class Brain:
         return self.session.run(self.out_value, feed_dict = {self.s_t: s})
 
 class Agent:
-    def __init__(self, eps_start, eps_end, eps_steps):
+    def __init__(self, eps_start, eps_end, eps_steps, frames):
         self.eps_start = eps_start
         self.eps_end = eps_end
         self.eps_steps = eps_steps
 
         self.memory = [] # for n-step return
         self.R = 0.
-        self.frames = 0
+        self.frames = frames
+        print("Starting frames: ", frames)
 
     def get_eps(self):
         if self.frames >= self.eps_steps:
@@ -272,62 +263,83 @@ class Agent:
             return a
     
     def train(self, s, a, r, s_, done):
-        # I haven't gone through the code yet, just assumed it works
-        def get_sample(memory, n):
-            s, a, _, _ = memory[0]
-            _, _, _, s_ = memory[n - 1]
-
-            return s, a, self.R, s_
+        self.frames += 1
 
         a_onehot = np.zeros(NUM_ACTIONS)
         a_onehot[a] = 1
 
         self.memory.append( (s, a_onehot, r, s_) )
 
-        self.R = (self.R + r * GAMMA_N) / GAMMA
+        if done or self.frames >= self.t_start + T_MAX:
+            R = 0
+            if not done:
+                R = brain.predict_v(np.array([s]))[0]
 
-        if done:
-            while len(self.memory) > 0:
-                n = len(self.memory)
-                s, a, r, s_ = get_sample(self.memory, n)
-                brain.train_push(s, a, r, s_, done)
+            for mem in reversed(self.memory):
+                R = mem[2] + GAMMA * R
+                brain.train_push(mem[0], mem[1], R)
 
-                self.R = (self.R - self.memory[0][2]) / GAMMA
-                self.memory.pop(0)
+            self.memory = []
+            self.t_start = self.frames + 1
 
-            self.R = 0
-        
-        if len(self.memory) >= N_STEP_RETURN:
-            n = N_STEP_RETURN
-            s, a, r, s_ = get_sample(self.memory, n)
-            brain.train_push(s, a, r, s_, done)
+class Pipeline:
 
-            self.R = self.R - self.memory[0][2]
-            self.memory.pop(0)
+    @staticmethod
+    def pipeline(image, new_HW=(80, 80), height_range=(35, 193), bg=(144, 72, 17)):
+        image = Pipeline.crop_image(image, height_range)
+        image = Pipeline.resize_image(image, new_HW)
+        image = Pipeline.kill_background_grayscale(image, bg)
+        image = np.expand_dims(image, axis=2)
+
+        return image
+
+    @staticmethod
+    def resize_image(image, new_HW):
+        return imresize(image, new_HW, interp="nearest")
+
+    @staticmethod
+    def crop_image(image, height_range=(35, 195)):
+        h_beg, h_end = height_range
+        return image[h_beg:h_end, ...]
+
+    @staticmethod
+    def kill_background_grayscale(image, bg):
+        H, W, _ = image.shape
+
+        R = image[..., 0]
+        G = image[..., 1]
+        B = image[..., 2]
+
+        cond = (R == bg[0]) & (G == bg[1]) & (B == bg[2])
+
+        image = np.zeros((H, W))
+        image[~cond] = 1
+
+        return image
 
 class Environment(threading.Thread):
-    def __init__(self, render = False, eps_start = EPS_START, eps_end = EPS_STOP, eps_steps = EPS_STEPS):
+    def __init__(self, render = False, eps_start = EPS_START, eps_end = EPS_STOP, eps_steps = EPS_STEPS, frames = 0):
         threading.Thread.__init__(self)
 
         self.stop_signal = False
         self.render = render
         self.env = gym.make(ENV)
-        self.agent = Agent(eps_start, eps_end, eps_steps)
-        self.phi = np.zeros([84, 84, 4])
+        self.agent = Agent(eps_start, eps_end, eps_steps, frames)
+        self.phi = np.zeros([80, 80, 4])
         self.s_prev = np.zeros([210, 160, 3])
 
         self.R_list = [] # for plotting
 
-    def process_image(self, s, s_prev):
+    def process_image(self, s):
         """
-        Processes image by converting from [210, 160, 3] -> [84, 84]
+        Processes image by converting from [210, 160, 3] -> [80, 80]
         skimage just does all this lol
         thanks random github guy
         """
 
-        s = np.maximum(s, s_prev)
-        return resize(rgb2gray(s), (110, 84))[13:97, :]
-    
+        s = np.maximum(s, self.s_prev)
+        return Pipeline.pipeline(s)
+
     def update_phi(self, s):
         """
         Updates phi with new observation.
@@ -335,12 +347,12 @@ class Environment(threading.Thread):
         we rollback phi and insert s (like a queue).
         """
 
-        s_new = np.reshape(self.process_image(s, self.s_prev), (84, 84, 1))
+        s_new = self.process_image(s)
         self.phi = np.concatenate([self.phi[:, :, 1:], s_new], axis = 2)
         self.s_prev = s
 
     def run_episode(self):
-        self.phi = np.zeros([84, 84, 4])
+        self.phi = np.zeros([80, 80, 4])
         s = self.env.reset()
         self.update_phi(s)
 
@@ -351,13 +363,14 @@ class Environment(threading.Thread):
             a = self.agent.act(self.phi)
 
             for i in range(FRAME_SKIP): # perform a for FRAME_SKIP times
-                s_, r, done, info = self.env.step(a)
+                s_, r, done, info = self.env.step(a + 1)
+                if r > 1: r = 1
+                elif r < -1: r = -1
                 phi_bef = self.phi
                 self.update_phi(s_)
 
                 if self.render:
                     self.env.render()
-                    #pass
                 else:
                     self.agent.train(phi_bef, a, r, self.phi, done)
 
@@ -402,26 +415,37 @@ def main(unused_argv):
     global brain
     brain = Brain() 
 
-    envs = [Environment(eps_end = np.random.choice(EPS_STOP_LIST, p = EPS_STOP_DIST)) for i in range(THREADS)]
+    frames = 0 # starting frames
+    if os.path.isdir(SAVE_DIR):
+        ckpt = tf.train.latest_checkpoint(SAVE_DIR)
+        frames = int(ckpt[len(SAVE_PARAM_PATH) + 1:]) / THREADS # get number of latest checkpoint
+
+    envs = [Environment(eps_end = np.random.choice(EPS_STOP_LIST, p = EPS_STOP_DIST), frames = frames) for i in range(THREADS)]
     opts = [Optimizer() for i in range(OPTIMIZERS)]
 
-    #we run all of these guys in parallel
-    for o in opts: o.start()
-    for e in envs: e.start()
-    #env_test.start()
+    # we run all of these guys in parallel
+    if RUN_TRAIN:
+        for o in opts: o.start()
+        for e in envs: e.start()
+    if SHOW_ENV_TEST: env_test.start()
 
-    time.sleep(RUN_TIME)
+    while True:
+        a = input()
+        if a == 'quit':
+            break
 
-    for e in envs: e.stop()
-    for e in envs: e.join()
+    if RUN_TRAIN:
+        for e in envs: e.stop()
+        for e in envs: e.join()
 
-    for o in opts: o.stop()
-    for o in opts: o.join()
-    #env_test.stop()
-    #env_test.join()
+        for o in opts: o.stop()
+        for o in opts: o.join()
+
+    if SHOW_ENV_TEST:
+        env_test.stop()
+        env_test.join()
 
     print("Training finished")
-    #env_test.run()
 
     '''
     with open('R_data', 'wb') as f:
